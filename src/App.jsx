dcf97c0e1906464314d3c69462a5918f6377589a
Ben Brownsell <ben.brownsell@hotmail.co.uk>
2023-05-03 23:14:13 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import Title from "./Title";
+import Menu from "./Menu";
+import menu from "./data";
+import { useState } from "react";
+
+const tempCategories = menu.map((item) => item.category);
+const tempSet = new Set(tempCategories);
+const tempItems = ["all", ...tempSet];
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
+console.log(allCategories);
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
