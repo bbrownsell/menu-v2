7baf86231ade0bac8e789070a9637ebb0248a0eb
Ben Brownsell <ben.brownsell@hotmail.co.uk>
2023-05-09 22:51:31 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Title from "./Title";
 import Menu from "./Menu";
 import menu from "./data";
 import { useState } from "react";
+import Categories from "./Categories";
 
 const tempCategories = menu.map((item) => item.category);
 const tempSet = new Set(tempCategories);
@@ -14,10 +15,21 @@ console.log(allCategories);
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <main>
       <section className="menu">
         <Title text="our menu" />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
